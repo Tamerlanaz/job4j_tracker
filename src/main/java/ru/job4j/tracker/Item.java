5fa6a8e8a52404b758e43abfaf101fc63aa8258d
Tamerlanaz <tamerlanaz@gmail.com>
2022-05-17 14:49:52 +0300

    7. Date. Отображение даты. [#310017 #301703]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -7,10 +7,6 @@ public class Item {
     private String name;
     private LocalDateTime created = LocalDateTime.now();
 
-    public LocalDateTime getCreated() {
-        return created;
-    }
-
     public Item() {
     }
 
@@ -21,6 +17,10 @@ public class Item {
     public Item(String name, int id) {
         this.name = name;
         this.id = id;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 
     public int getId() {
