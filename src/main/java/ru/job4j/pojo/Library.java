e1e5b9be4dbf4e4b3d5952a271ed99f00333738d
Tamerlanaz <tamerlanaz@gmail.com>
2022-05-19 12:52:46 +0300

    3. Массивы и модели. [#174892 #302504]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -27,7 +27,7 @@ public class Library {
         System.out.println("Выводим книги с названием \"Clean code\":");
         for (int index = 0; index < books.length; index++) {
             Book bk = books[index];
-            if (bk.equals(books[1])) {
+            if ("Clean code".equals(bk.getName())) {
                 System.out.println(bk.getName());
             }
         }
